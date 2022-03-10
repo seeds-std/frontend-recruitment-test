9d2ca2bd38afffb36df7aeccb3159b9f27c97f33
fumiya yasojima <yasojima@seeds-std.co.jp>
2022-03-10 16:25:24 +0900

    テスト作業用のコンポーネントとして切り出し

---
+++ src/components/Playground/Part-1.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+//  chakra
+import { Box, Input, Text } from "@chakra-ui/react";
+
+const Playground = () => {
+  const [messege, setMessege] = useState<string>("");
+
+  const onChangeMessege = (e) => {
+    setMessege(e.target.value);
+  };
+
+  return (
+    <>
+      <Box mb={4} textAlign="center">
+        <Text fontSize="sm" color="GrayText">
+          入力メッセージ
+        </Text>
+        <Text fontSize="lg" fontWeight="bold">
+          {messege || "メッセージを入れてください！"}
+        </Text>
+      </Box>
+
+      <Input placeholder="メッセージ" size="md" onChange={onChangeMessege} />
+    </>
+  );
+};
+
+export default Playground;
