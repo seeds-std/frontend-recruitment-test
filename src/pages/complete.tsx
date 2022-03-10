c3aef94fcf7646d9c267d516bf9881a9a831e2d8
fumiya yasojima <yasojima@seeds-std.co.jp>
2022-03-10 17:04:06 +0900

    軽微な調整

--- src/pages/complete.tsx
+++ src/pages/complete.tsx
@@ -5,13 +5,18 @@ import Layout from "components/Layout";
 import ButtonWrapper from "components/ButtonWrapper";
 import ArrowBackButton from "components/ArrowBackButton";
 
+//  chakra
+import { Box } from "@chakra-ui/react";
+
 const Complete = () => {
   return (
     <Layout title="お疲れさまでした！">
       テストは以上です。お疲れさまでした。
-      <ButtonWrapper justifyContent="center">
-        <ArrowBackButton href="/">トップに戻る</ArrowBackButton>
-      </ButtonWrapper>
+      <Box mt={12}>
+        <ButtonWrapper justifyContent="center">
+          <ArrowBackButton href="/">トップに戻る</ArrowBackButton>
+        </ButtonWrapper>
+      </Box>
     </Layout>
   );
 };
