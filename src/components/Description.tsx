32fd2e4c9b285b9b65ee89552709b5639f7944a7
fumiya yasojima <yasojima@seeds-std.co.jp>
2022-03-10 16:48:26 +0900

    エラー修正

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -6,7 +6,7 @@ import { Box, Text } from "@chakra-ui/react";
 const Description = ({ children }) => {
   return (
     <Box bgColor="gray.50" borderRadius={3} p={4}>
-      <Text>{children}</Text>
+      {children}
     </Box>
   );
 };
