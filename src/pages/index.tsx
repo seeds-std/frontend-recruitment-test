41a6ece298b79ac19fe96e984422fc7ee0d3cafc
fumiya yasojima <yasojima@seeds-std.co.jp>
2022-03-11 19:47:47 +0900

    「はじめにの」文言の差し込み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 // chakra
-import { Box, Button, UnorderedList, ListItem, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  UnorderedList,
+  ListItem,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 
 // component
 import Layout from "components/Layout";
@@ -16,22 +23,26 @@ const Home = () => {
   return (
     <Layout title="はじめに">
       <Box>
-        <UnorderedList>
-          <ListItem>
-            注意事項テキストが入ります。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-          </ListItem>
-          <ListItem>
-            注意事項テキストが入ります。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-          </ListItem>
-          <ListItem>
-            注意事項テキストが入ります。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-          </ListItem>
-          <ListItem>
-            注意事項テキストが入ります。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-          </ListItem>
+        <Text>
+          株式会社シーズのフロントエンド試験にご参加いただきありがとうございます。
+          <br />
+          本試験は以下の内容を問う事を想定した問題となっています。
+        </Text>
+        <UnorderedList my={4}>
+          <ListItem>Reactの基礎</ListItem>
+          <ListItem>配列操作/state管理</ListItem>
+          <ListItem>コンポーネント設計</ListItem>
         </UnorderedList>
+        <Text>
+          テストを開始すると問題が表示されます。
+          <br />
+          作業ファイルを修正して指示された動作を実装してください。
+        </Text>
+        <Text mt={3}>
+          準備ができましたら「テストを開始する」をクリックしてください。
+        </Text>
       </Box>
-      <Flex justifyContent="center" mt={10}>
+      <Flex justifyContent="center" mt={12}>
         <Link href="/test/part-1" passHref>
           <Button colorScheme="teal" size="lg" rightIcon={<ArrowForwardIcon />}>
             テストを開始する
